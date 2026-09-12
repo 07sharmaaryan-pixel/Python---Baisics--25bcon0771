38e33e14ae09149c747cd6d9a918204d20eac89b
07sharmaaryan-pixel <07.sharmaaryan@gmail.com>
2026-09-12 09:32:37 +0530

    Add files via upload

---
+++ factorial (2).py
@@ -0,0 +1,15 @@
+# Factorial program in Python
+
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
+
+
+num = int(input("Enter a non-negative integer: "))
+print("Factorial:", factorial(num))
